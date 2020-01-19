52e420382ed6306912ea05a2833c1b70903355eb
Niklas Riekenbrauck <nr@mtomady.com>
2020-01-19 15:30:36 +0100

    Improve output and add CLI config

--- src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
+++ src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
@@ -1,29 +1,33 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.DecisionTreeClassificationModel
-import org.apache.spark.ml.classification.DecisionTreeClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-// A Scala case class; works out of the box as Dataset type using Spark's implicit encoders
-case class Person(name:String, surname:String, age:Int)
-
-// A non-case class; requires an encoder to work as Dataset type
-class Pet(var name:String, var age:Int) {
-  override def toString = s"Pet(name=$name, age=$age)"
-}
 
 object SimpleSpark extends App {
 
   override def main(args: Array[String]): Unit = {
-
     // Turn off logging
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
+
+    type OptionMap = Map[Symbol, Any]
+
+    def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
+      list match {
+        case Nil => map
+        case "--path" :: value :: tail =>
+          nextOption(map ++ Map('path -> value), tail)
+        case "--cores" :: value :: tail =>
+          nextOption(map ++ Map('cores -> value.toInt), tail)
+        case option :: tail =>
+          println("Unknown option "+option);
+          System.exit(1)
+          Map()
+      }
+    }
+    val options = nextOption(Map(), args.toList)
 
     //------------------------------------------------------------------------------------------------------------------
     // Setting up a Spark Session
@@ -33,7 +37,7 @@ object SimpleSpark extends App {
     val sparkBuilder = SparkSession
       .builder()
       .appName("Sindy")
-      .master("local[4]") // local, with 4 worker cores
+      .master(s"local[${options.getOrElse('cores, 4) }]") // local, with 4 worker cores
     val spark = sparkBuilder.getOrCreate()
 
     // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
@@ -52,7 +56,7 @@ object SimpleSpark extends App {
     }
 
     val inputs = List("region", "nation", "supplier", "customer", "part", "lineitem", "orders")
-      .map(name => s"data/TPCH/tpch_$name.csv")
+      .map(name => s"${options.getOrElse('path, "./TPCH")}/tpch_$name.csv")
 
     time { Sindy.discoverINDs(inputs, spark) }
   }

58d91b70d3fd267e2b6907e29b8738677422f5a9
Niklas Riekenbrauck <nr@mtomady.com>
2020-01-26 22:46:54 +0100

    Finally fix algorithm

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -16,7 +16,7 @@ object Sindy {
                        attributeSet: AttributeSet)
 
   case class InclusionList(attribute: Attribute,
-                           attributeSet: Seq[AttributeSet])
+                           attributeSets: Seq[AttributeSet])
 
   case class Value(value: String,
                    attribute: Attribute)
@@ -37,6 +37,8 @@ object Sindy {
 
     import spark.implicits._
 
+    // TODO: Add partinoniong here: p(v) = hash(v) mod n
+
     // Cells contains a dataframe of ALL distinct values per column + attribute name
     val cells = dataframes.map(df => {
       // Get the columns names and use them to select respective columns from the dataframe
@@ -51,28 +53,31 @@ object Sindy {
       .groupBy(cells.columns(0))
       .agg(collect_set(cells.columns(1)))
 
+    groupedAttributes.show(400)
+
     val attributeSets = groupedAttributes.select(groupedAttributes.columns(1)).as[AttributeSet]
 
     // Build inclusion list from attribute sets
     val inclusionLists = attributeSets.flatMap(inclusion => {
         // Generate set e.g [a,b] => [a, [b]], [b, [a]]
         inclusion.map(attribute => (attribute, inclusion.filter(_ != attribute)))
-      }).filter(inclusion => inclusion._2.nonEmpty)
-      .toDF("attribute", "attributeSet")
-        .as[Inclusion]
+      }).toDF("attribute", "attributeSet")
+        .as[Inclusion];
 
     // Build result list by collecting attribute sets and
     // merging them using the intersection
     val results = inclusionLists
       .groupBy(inclusionLists.columns(0))
       .agg(collect_set(inclusionLists.columns(1)))
-      .toDF("attribute", "attributeSet")
+      .toDF("attribute", "attributeSets")
       .as[InclusionList]
-      .map(inclusionList => (inclusionList.attribute, inclusionList.attributeSet.flatten.distinct))
+      .filter(inclusionLists => {
+        !inclusionLists.attributeSets.exists(e => e.isEmpty)
+      })
+      .map(inclusionList => (inclusionList.attribute, inclusionList.attributeSets.reduce((a,b) => a.intersect(b))))
       .toDF("attribute", "attributeSet")
-      .orderBy(org.apache.spark.sql.functions.col("attribute").asc)
       .as[Inclusion]
-
+      .filter(inclusionList => inclusionList.attributeSet.nonEmpty)
 
     // Print results in desired format
     results.foreach(result => println(s"${result.attribute} < ${result.attributeSet.mkString(",")}"))
